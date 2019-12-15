8c8117ded6fab6b2c965a0d99bc6f1917dac2fad
Dheeraj <kontham.dheeraj@gmail.com>
2019-12-15 10:44:13 +0530

    zipkin initial setup

---
+++ src/main/java/com/projects/springbootzipkin/SpringBootZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springbootzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZipkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/springbootzipkin/SpringBootZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.springbootzipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
